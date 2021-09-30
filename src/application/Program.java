43c9ff096eccd0c5ed26109e4953065aaa1ec83d
matheronn <matheusem94@gmail.com>
2021-09-29 22:21:40 -0300

    Projeto finalizado

---
+++ src/application/Program.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+
+		int[][] mat = new int[m][n];
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+
+		int select = sc.nextInt();
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (mat[i][j] == select) {
+					System.out.println("Position: " + i + "," + j);
+					if (i > 0) {
+						System.out.println("up: " + mat[i - 1][j]);
+					}
+					if (j > 0) {
+						System.out.println("left: " + mat[i][j - 1]);
+					}
+					if (j < mat[i].length - 1) {
+						System.out.println("right: " + mat[i][j + 1]);
+					}
+					if (i < mat.length - 1) {
+						System.out.println("down: " + mat[i + 1][j]);
+					}
+				}
+			}
+		}
+
+		sc.close();
+	}
+}
